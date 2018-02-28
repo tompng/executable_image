24c5dbfbda8dfb3ea8f077abe8627ea86012b9bd
tompng <tomoyapenguin@gmail.com>
2018-02-28 22:52:38 +0900

    compile code.pl

--- generator.rb
+++ generator.rb
@@ -25,13 +25,11 @@ code_eval = %(
     $e.=chr$d;
   }
 ).lines.map(&:strip).join
-code_eval += "exec'ruby','-e',$e"
-# code_eval += "eval$e"
+code_eval += "print$e"
 code_end = "\nEOF\n#{code_eval}"
 
 image = ChunkyPNG::Image.from_file 'input.png'
-code = File.read 'code.rb'
-# code = File.read 'code.pl'
+code = File.read 'code.pl'
 
 w = 68
 h = 44
